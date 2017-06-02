b8e69d94b4b6ae6a11dab70d782b9b02ba440dde
viv-iam <vivian.opondoh@gmail.com>
2017-06-02 15:04:37 +0300

    Write tests
    *Local test to ensure our array has total number of restaurant names we added
    *Instrumentation test to ensure our toast instantiates with the correct restaurant name

--- app/src/androidTest/java/com/innovach/myrestaurants/RestaurantsActivityInstrumentationTest.java
+++ app/src/androidTest/java/com/innovach/myrestaurants/RestaurantsActivityInstrumentationTest.java
@@ -1,8 +1,39 @@
 package com.innovach.myrestaurants;
 
-/**
- * Created by vivian on 02/06/17.
- */
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.core.IsNot.not;
 
 public class RestaurantsActivityInstrumentationTest {
+
+    //Rule to begin RestaurantsActivity before each test
+    @Rule
+    public ActivityTestRule<RestaurantsActivity> activityTestRule =
+            new ActivityTestRule<>(RestaurantsActivity.class);
+
+    //asserting toast instantiates with correct restaurant name
+    @Test
+    public void listItemClickDisplaysToastWithCorrectRestaurant() {
+        View activityDecorView = activityTestRule.getActivity().getWindow().getDecorView();
+        String restaurantName = "Mi Mero Mole";
+        onData(anything())
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(0)
+                .perform(click());
+        onView(withText(restaurantName)).inRoot(withDecorView(not(activityDecorView)))
+                .check(matches(withText(restaurantName)));
+    }
+
 }
