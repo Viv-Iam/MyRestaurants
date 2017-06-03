172fffac53d94e2a9a5ec7a5f32372c801a99559
viv-iam <vivian.opondoh@gmail.com>
2017-06-03 13:41:30 +0300

    Add new class MyRestaurantsArrayAdapter

---
+++ app/src/main/java/com/innovach/myrestaurants/MyRestaurantsArrayAdapter.java
@@ -0,0 +1,13 @@
+package com.innovach.myrestaurants;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyRestaurantsArrayAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_restaurants_array_adapter);
+    }
+}
--- app/src/main/java/com/innovach/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/innovach/myrestaurants/RestaurantsActivity.java
@@ -3,12 +3,16 @@ package com.innovach.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class RestaurantsActivity extends AppCompatActivity {
     @Bind(R.id.locationTextView) TextView mLocationTextView;
@@ -22,14 +26,13 @@ public class RestaurantsActivity extends AppCompatActivity {
             "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
             "Lardo", "Portland City Grill", "Fat Head's Brewery",
             "Chipotle", "Subway"};
-    public static final String TAG = RestaurantsActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
-        @ButterKnife.bind(this);
- );
+        ButterKnife.bind(this);
+
         //create new adapter
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
         //set ListView adapter to new adapter
