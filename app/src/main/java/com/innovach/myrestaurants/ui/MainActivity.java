0e89ff8a5c392e160868abe1b46e4d594745c42a
viv-iam <vivian.opondoh@gmail.com>
2017-06-06 14:49:37 +0300

    Add Yelp ConsumerKey, Yelp ConsumerSecret, Yelp Token and Yelp Token Secret

---
+++ app/src/main/java/com/innovach/myrestaurants/ui/MainActivity.java
@@ -0,0 +1,44 @@
+package com.innovach.myrestaurants.ui;
+        import butterknife.Bind;
+        import butterknife.ButterKnife;
+
+        import android.content.Intent;
+        import android.graphics.Typeface;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+
+        import com.innovach.myrestaurants.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrich-regular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
+
+        mFindRestaurantsButton.setOnClickListener(this);
+
+    }
+    public void onClick(View v) {
+        if(v == mFindRestaurantsButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+
+        }
+    }
+
+}
