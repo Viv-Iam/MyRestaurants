9bdb07a3968e28c616e885b6b382c005d2ea50f9
viv-iam <vivian.opondoh@gmail.com>
2017-06-12 20:16:59 +0300

    *Call built-in Firebase method createUserWithEmailAndPassword() mehtod *Fetch contents of registration in the createNewUser() method

--- app/src/main/java/com/innovach/myrestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/com/innovach/myrestaurants/ui/CreateAccountActivity.java
@@ -2,13 +2,19 @@
 package com.innovach.myrestaurants.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.innovach.myrestaurants.R;
 
@@ -46,5 +52,23 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
             createNewUser();
         }
     }
-    private void
+    private void createNewUser() {
+        final String name = mNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>(){
+
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "Authentication successful");
+                }else{
+                    Toast.makeText(CreateAccountActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+    }
 }
