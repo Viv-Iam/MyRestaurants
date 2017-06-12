cde8798bf65ca6a01d7dbbb8674bdc55a9b36376
viv-iam <vivian.opondoh@gmail.com>
2017-06-12 17:36:23 +0300

    Have to implement View.OnClickListener to execute onClick on a view

--- app/src/main/java/com/innovach/myrestaurants/ui/LoginActivity.java
+++ app/src/main/java/com/innovach/myrestaurants/ui/LoginActivity.java
@@ -2,14 +2,22 @@ package com.innovach.myrestaurants.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.innovach.myrestaurants.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity im{
+    @Bind(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
     }
 }
