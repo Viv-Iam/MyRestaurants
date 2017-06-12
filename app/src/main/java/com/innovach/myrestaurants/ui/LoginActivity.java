a1e45aa54b077cd47d0e1c26923d70a1a174d487
viv-iam <vivian.opondoh@gmail.com>
2017-06-12 11:25:32 +0300

    Create LoginActivity

---
+++ app/src/main/java/com/innovach/myrestaurants/ui/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.innovach.myrestaurants.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.innovach.myrestaurants.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
