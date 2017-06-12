28974fa4a6f960c64003fe968b1e3edaec925916
viv-iam <vivian.opondoh@gmail.com>
2017-06-12 22:18:29 +0300

    Declare var of FirebaseAuth.AuthStateListener object

--- app/src/main/java/com/innovach/myrestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/com/innovach/myrestaurants/ui/CreateAccountActivity.java
@@ -31,6 +31,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     @Bind(R.id.loginTextView) TextView mLoginTextView;
 
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
