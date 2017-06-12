50a2e4bbeaa32b14928921e3cf59b2f3d03dc4c3
viv-iam <vivian.opondoh@gmail.com>
2017-06-12 18:02:17 +0300

    Set onClickListener on signup text with mRegisterTextView id

--- app/src/main/java/com/innovach/myrestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/com/innovach/myrestaurants/ui/CreateAccountActivity.java
@@ -3,14 +3,32 @@ package com.innovach.myrestaurants.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.innovach.myrestaurants.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.createUserButton) Button mCreateUserButton;
+    @Bind(R.id.nameEditText) EditText mNameEditText;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @Bind(R.id.loginTextView) TextView mLoginTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+
+        mLoginTextView.setOnClickListener(this);
+        mCreateUserButton.setOnClickListener(this);
     }
 }
--- app/src/main/java/com/innovach/myrestaurants/ui/LoginActivity.java
+++ app/src/main/java/com/innovach/myrestaurants/ui/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.innovach.myrestaurants.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.innovach.myrestaurants.R;
@@ -9,7 +11,7 @@ import com.innovach.myrestaurants.R;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class LoginActivity extends AppCompatActivity im{
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
@@ -19,5 +21,13 @@ public class LoginActivity extends AppCompatActivity im{
 
         ButterKnife.bind(this);
         mRegisterTextView.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
