58b2d8b35a5414603ac635855b32304bfd058b49
viv-iam <vivian.opondoh@gmail.com>
2017-06-13 16:36:52 +0300

    *Use ButterKnife to bind EditTexts and Button views *Declare and set instance of FirebaseAuth

--- app/src/main/java/com/innovach/myrestaurants/ui/LoginActivity.java
+++ app/src/main/java/com/innovach/myrestaurants/ui/LoginActivity.java
@@ -4,15 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.innovach.myrestaurants.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = LoginActivity.class.getSimpleName();
+
+    @Bind(R.id.passwordLoginButton) Button mPasswordLoginButton;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
     @Bind(R.id.registerTextView) TextView mRegisterTextView;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +30,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
 
         ButterKnife.bind(this);
+        mAuth = FirebaseAuth.getInstance();
         mRegisterTextView.setOnClickListener(this);
     }
     @Override
