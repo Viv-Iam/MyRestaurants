d7de10e14d0f7f2d17b7b06ba2518fc9d9d7229a
viv-iam <vivian.opondoh@gmail.com>
2017-06-13 10:43:37 +0300

    Add ProgressDialog object to CreateAccountActivity *Add new member variable for ProgressDialog object *Call createAuthProgressDialog() method in the onCreate() method *Inside createAuthProgressDialog() set title and message values and setCancelable() to false *Show Progress Dialog in createNewUser() method by adding mAuthProgressDialog.show() *Dismiss the dialogue by calling mAuthProgressDialog.dismiss() in the createUserWithEmailandPassword()

--- app/src/main/java/com/innovach/myrestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/com/innovach/myrestaurants/ui/CreateAccountActivity.java
@@ -1,6 +1,7 @@
 
 package com.innovach.myrestaurants.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -33,6 +34,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private ProgressDialog mAuthProgressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +44,14 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         mLoginTextView.setOnClickListener(this);
         mCreateUserButton.setOnClickListener(this);
         createAuthStateListener();
+        createAuthProgressDialog();
+    }
+
+    private void createAuthProgressDialog() {
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
     }
     @Override
     public void onClick(View view) {
@@ -65,11 +75,13 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         boolean validName = isValidName(name);
         boolean validPassword = isValidPassword(password, confirmPassword);
         if (!validEmail || !validName || !validPassword) return;
+        mAuthProgressDialog.show();
 
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>(){
 
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+                mAuthProgressDialog.dismiss();
                 if (task.isSuccessful()) {
                     Log.d(TAG, "Authentication successful");
                 }else{
