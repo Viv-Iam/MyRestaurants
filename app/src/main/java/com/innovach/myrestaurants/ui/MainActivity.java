d3f7dd426d2ee8dfe4f9240e649a1b792d95d289
viv-iam <vivian.opondoh@gmail.com>
2017-06-13 04:18:45 +0300

    Add code to menu_main.xml and include it in MainActivity *Inflate new menu in an onCreateOptionsMenu() method *Include onOptionsItemSelected() override and add code to it *Add logout() method that only calls logout()

--- app/src/main/java/com/innovach/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/innovach/myrestaurants/ui/MainActivity.java
@@ -7,11 +7,15 @@ package com.innovach.myrestaurants.ui;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.util.Log;
+        import android.view.Menu;
+        import android.view.MenuInflater;
+        import android.view.MenuItem;
         import android.view.View;
         import android.widget.Button;
         import android.widget.EditText;
         import android.widget.TextView;
 
+        import com.google.firebase.auth.FirebaseAuth;
         import com.google.firebase.database.DataSnapshot;
         import com.google.firebase.database.DatabaseError;
         import com.google.firebase.database.DatabaseReference;
@@ -39,6 +43,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mFindRestaurantsButton.setOnClickListener(this);
         mSavedRestaurantsButton.setOnClickListener(this);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
     }
 
     @Override
