e11e46ec685f39d592019bb96a1dc442c2092baa
viv-iam <vivian.opondoh@gmail.com>
2017-06-14 07:35:50 +0300

    Add buildt-in Firebase method =>signInWithEMailAndPassword() within loginWithPassword in LoginActivity

--- app/src/main/java/com/innovach/myrestaurants/ui/LoginActivity.java
+++ app/src/main/java/com/innovach/myrestaurants/ui/LoginActivity.java
@@ -1,13 +1,19 @@
 package com.innovach.myrestaurants.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.innovach.myrestaurants.R;
 
@@ -23,6 +29,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Bind(R.id.registerTextView) TextView mRegisterTextView;
 
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +65,19 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 mPasswordEditText.setError("Password cannot be blank");
                 return;
             }
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                            if (!task.isSuccessful()) {
+                                Log.w(TAG, "signInWithEmail", task.getException());
+                                Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+
         }
     }
 
